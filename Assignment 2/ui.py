12e6d6a3104d367e3b8dad938532a1093851353e
rosemchen <chenro@oregonstate.edu>
2022-04-11 10:07:33 -0700

    adding comments

--- Assignment 2/ui.py
+++ Assignment 2/ui.py
@@ -1,6 +1,6 @@
 # A user interface (UI) that either has a button or can receive a user command. When
 # the button is pushed or the command is entered...
 # a.    UI calls the PRNG Service
-# b.     UI calls the Image Service using the psuedo-random number from the PRNG Service
+# b.    UI calls the Image Service using the psuedo-random number from the PRNG Service
 # c.    UI displays the image (or a path to it)
 
