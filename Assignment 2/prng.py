6eafae2432a2ec269cd5f8d2aaa1160514257fe1
rosemchen <chenro@oregonstate.edu>
2022-04-11 11:15:29 -0700

    prng file

--- Assignment 2/prng.py
+++ Assignment 2/prng.py
@@ -1,1 +1,18 @@
 # Program that generates psuedo-random numbers (PRNG service)
+
+import random
+import time
+
+random.seed()
+
+while True:
+    time.sleep(1,0)
+
+    prng = open('prng-service.txt', 'r')
+    entry = prng.readline()
+
+    if entry is 'run':
+        randomNum = str(random.randint[1,3])
+        prng.seek(0)
+        prng.write(randomNum)
+    prng.close()
